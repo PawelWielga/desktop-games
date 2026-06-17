02b6fc027e5c7abe5a0c3a7043a86487f0c41cde
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-06-17 13:21:17 +0200

    feat: export multiplayer module

---
+++ src/multiplayer/index.ts
@@ -0,0 +1,17 @@
+export { createRoomCode, isValidRoomCode, normalizeRoomCode, PeerRoomConnection } from "./peerConnection";
+export { usePeerRoom } from "./usePeerRoom";
+export type {
+  JsonObject,
+  JsonPrimitive,
+  JsonValue,
+  MultiplayerConnectionError,
+  MultiplayerConnectionStatus,
+  MultiplayerErrorHandler,
+  MultiplayerMessage,
+  MultiplayerMessageHandler,
+  MultiplayerRole,
+  MultiplayerStatusHandler,
+  PeerRoomConnectionOptions,
+  PeerRoomSnapshot,
+} from "./multiplayer.types";
+export type { UsePeerRoomOptions, UsePeerRoomResult } from "./usePeerRoom";
