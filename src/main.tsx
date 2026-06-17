03ceb7b37afc7b1f17a53168a28e8a9397527d42
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-06-17 15:59:04 +0200

    Remove unused routing layout (#140)

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-﻿import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/base.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
