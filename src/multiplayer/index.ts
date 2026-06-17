d2f1c5e97af012be5e6b4d8ff62b4b9f1f0d65ad
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-06-17 13:53:15 +0200

    export multiplayer lobby and room APIs

--- src/multiplayer/index.ts
+++ src/multiplayer/index.ts
@@ -1,5 +1,19 @@
 export { createRoomCode, isValidRoomCode, normalizeRoomCode, PeerRoomConnection } from "./peerConnection";
 export { usePeerRoom } from "./usePeerRoom";
+export { MultiplayerPanel } from "./MultiplayerPanel";
+export { useMultiplayerLobby } from "./useMultiplayerLobby";
+export { usePeerHostRoom } from "./usePeerHostRoom";
+export { usePeerClientRoom } from "./usePeerClientRoom";
+export {
+  createMessage,
+  isMessageOfType,
+  isPlayerHelloMessage,
+  isPlayerProfile,
+  isRoomPlayersMessage,
+  onMessage,
+  parseMultiplayerMessage,
+  sendMessage,
+} from "./messages";
 export type {
   JsonObject,
   JsonPrimitive,
@@ -15,3 +29,34 @@ export type {
   PeerRoomSnapshot,
 } from "./multiplayer.types";
 export type { UsePeerRoomOptions, UsePeerRoomResult } from "./usePeerRoom";
+export type {
+  AnyMultiplayerMessage,
+  BaseMultiplayerMessage,
+  CommonMultiplayerMessage,
+  GameErrorMessage,
+  GameReadyMessage,
+  GameResetMessage,
+  GameSpecificMessage,
+  MessageMetadata,
+  MessageSend,
+  PlayerHelloMessage,
+  PlayerProfile,
+  RoomPlayersMessage,
+} from "./messages";
+export type {
+  GameSessionRole,
+  GameSessionSnapshot,
+  LocalMultiplayerPlayer,
+  RemotePlayer,
+  TurnOwner,
+} from "./gameSession.types";
+export type { MultiplayerLobbyStatus, UseMultiplayerLobbyOptions, UseMultiplayerLobbyResult } from "./useMultiplayerLobby";
+export type {
+  HostClientRoomEvent,
+  HostClientRoomMessage,
+  HostClientRoomOptions,
+  HostClientRoomStatus,
+  RemotePlayer as HostClientRemotePlayer,
+} from "./hostClientRoom.types";
+export type { UsePeerHostRoomResult } from "./usePeerHostRoom";
+export type { UsePeerClientRoomResult } from "./usePeerClientRoom";
